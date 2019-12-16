413abb43900e781169e65136adc808e7291d1a5e
bekmezci69 <58048928+bekmezci69@users.noreply.github.com>
2019-12-16 19:19:15 +0100

    Dashboard Inside Component Refactoring

--- src/app/dashboard/dashboard-inside/dashboard-inside.component.ts
+++ src/app/dashboard/dashboard-inside/dashboard-inside.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, Attribute, Input} from '@angular/core';
 import {SearchService} from "../../../app/core/services/search.service";
+import {DashboardItem} from './dashboardItem';
 
 @Component({
   selector: 'app-dashboard-inside',
@@ -7,21 +8,27 @@ import {SearchService} from "../../../app/core/services/search.service";
   styleUrls: ['./dashboard-inside.component.scss']
 })
 export class DashboardInsideComponent implements OnInit {
-  items = [];
-  @Input() bereichvalue: string;
+  //Gesamtergebnisse speichern für UI
+  dashboardItems:Array<DashboardItem> = new Array<DashboardItem>() ;
+  //Input von übergeordneten Component(Dashboard)
+  @Input() dashboardArea: string;
 
-  constructor(private _service: SearchService) {
+  constructor(private service: SearchService) {
 
   }
 
   ngOnInit() {
-    var isKeyWheather:boolean = this.bereichvalue == "weather";
-    var countofData:number =  isKeyWheather ? 5 : 10;
-    var result:string[] = new Array();
-    var weatherarray:string[] = new Array();
-    var listOfStackoverflow:string[] = new Array();
+    this.FillData4UI();
 
-    this._service.search(this.bereichvalue).subscribe( res => {
+  }
+
+  FillData4UI() : void {
+    var isAreaWheater:boolean = this.dashboardArea == "weather";
+    //Gesamt Ergebnisse zwischenspeichern
+    var result:Array<DashboardItem> = new Array<DashboardItem>();
+
+    //Über den jeweiligen Dashboard Bereich Susbscriben
+    this.service.search(this.dashboardArea).subscribe( res => {
       var items  = res['items']
         .sort(
           function(a, b) {
@@ -29,33 +36,41 @@ export class DashboardInsideComponent implements OnInit {
             b = new Date(b.creation_date.dateModified);
             return a.creation_date > b.creation_date ? -1 : a.creation_date < b.creation_date ? 1 : 0;
           }
-        ).slice(0, countofData);
+          //Array größe festlegen anhand des bereiches
+        ).slice(0, isAreaWheater ? 5 : 10);
+
+      var listOfweather:string[] = new Array<string>();//Wetterdaten zwieschenspeichern
+      var listOfStackoverflow:Array<DashboardItem> = new Array<DashboardItem>();//Stackoverflow daten zwischenspeichern
 
       items.forEach(function (value) {
-        if(isKeyWheather) {
-          listOfStackoverflow.push(value.title);
+        if(isAreaWheater) {
+          listOfStackoverflow.push(new DashboardItem(value.title, value.link));
         }else {
-          result.push(value.title);
+          result.push(new DashboardItem(value.title, value.link));
         }
       });
-      if(isKeyWheather){
-        this._service.search("weatherdata").subscribe((data) => {
+
+      if(isAreaWheater){
+        //Daten aus Wetterdatenbank auslesen
+        this.service.search("weatherdata").subscribe((data) => {
             for (var key in data) {
-              var wetter:string = "Wetter am " + data[key].Datum + " "+ data[key]['Temp. A.'] + " / "+ data[key]['Temp. 3'] + " C°";
-              weatherarray.push(wetter);
+              listOfweather.push("Wetter am " + data[key].Datum + " "+ data[key]['Temp. A.'] + " / "+ data[key]['Temp. 3'] + " C°");
             }
+
             for(var i=0; i <= 4;i++) {
-              result.push(listOfStackoverflow[i]);
-              result.push(weatherarray[i]);
+              // Wetterdaten von Stackoverflow und von Wetterdatenbank abwechselnd ins result zwischenspeichern
+              result.push(new DashboardItem(listOfStackoverflow[i].title, listOfStackoverflow[i].link));
+              result.push(new DashboardItem(listOfweather[i], ""));
             }
           }
         );
 
       }
+      //Zwischengespeicherten result ins dashboarditems übergeben um im HTML zu verwenden.
+      this.dashboardItems = result;
 
     }, (error)  => console.error(error));
 
-    this.items = result;
   }
 
 
